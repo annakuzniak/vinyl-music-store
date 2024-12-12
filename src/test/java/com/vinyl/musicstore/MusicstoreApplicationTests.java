a953bc41051d904e7b67ac3662f1afdf63af8cd8
Anna Kuźniak <annakuzniak@MacBook-Pro-Anna.local>
2024-12-12 15:42:04 +0100

    Initial commit

---
+++ src/test/java/com/vinyl/musicstore/MusicstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinyl.musicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
